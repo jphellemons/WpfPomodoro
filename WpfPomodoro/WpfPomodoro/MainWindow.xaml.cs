75f090b526d54606852c451bd461889d601534bc
jphellemons <info@jphellemons.nl>
2012-08-07 20:37:32 +0200

    build 2
    another build. next build will have taskbar thumb buttons. better gui
    and online task integration

--- WpfPomodoro/WpfPomodoro/MainWindow.xaml.cs
+++ WpfPomodoro/WpfPomodoro/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfPomodoro
 {
@@ -22,42 +23,61 @@ namespace WpfPomodoro
     /// </summary>
     public partial class MainWindow : Window
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        //private TimeSpan _ts = new TimeSpan(0, 0, 25, 0);
+        private TimeSpan _ts = new TimeSpan(0, 0, 0, 30);
+        private int _totalSeconds = 30;
+        //private int _totalSeconds = 1500;
+        private DispatcherTimer _counter;
 
-        private Timer t;
-        private TimeSpan ts = new TimeSpan(0,0,25,0);
         public MainWindow()
         {
             InitializeComponent();
-            t = new Timer(1000);
-            t.Elapsed += t_Elapsed;
-        }
-
-        public string Counter
-        {
-            get {
-                OnPropertyChanged(new PropertyChangedEventArgs("Counter"));
-
-                return ts.ToString();
-            }
-        }
-
-        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
-        {
-            if (null != PropertyChanged)
-            {
-                PropertyChanged(this, e);
-            }
+            TaskbarItemInfo.ProgressState = System.Windows.Shell.TaskbarItemProgressState.Normal;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            t.Start();
+            _counter = new DispatcherTimer();
+            _counter.Tick += new EventHandler(counterOne_Tick);
+            _counter.Interval = new TimeSpan(0, 0, 0, 1);
+
+            _counter.Start();
+
+            //http://msdn.microsoft.com/en-us/library/system.windows.media.mediaplayer.volume.aspx
+            MediaPlayer mp = new MediaPlayer();
+            mp.Volume = 1.0;
+            mp.Open(new Uri("Sounds/wc3-peon-work-work.mp3", UriKind.Relative));
+            mp.Play();
         }
 
-        void t_Elapsed(object sender, ElapsedEventArgs e)
+        private void counterOne_Tick(object sender, EventArgs e)
         {
-            ts = ts.Add(new TimeSpan(0, 0, 0, -1));
+            _ts = _ts.Add(new TimeSpan(0, 0, 0, -1));
+            time.Text = string.Format("{0}:{1}", _ts.Minutes, _ts.Seconds);
+            TaskbarItemInfo.ProgressValue = (((_totalSeconds - _ts.TotalSeconds) * 100) / _totalSeconds) / 100;
+
+            if(_ts <= new TimeSpan(0,0,0))
+            {
+                TaskbarItemInfo.ProgressValue = 1.0;
+                TaskbarItemInfo.ProgressState = System.Windows.Shell.TaskbarItemProgressState.Indeterminate;
+
+                _counter.Stop();
+
+                MediaPlayer mp = new MediaPlayer();
+                mp.Open(new Uri("Sounds/ring.wav", UriKind.Relative));
+                mp.Play();
+            }
+        }
+
+        private void MainWindow_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
